e2fdc7687fce0abec942f18882870f7c939290d2
DarvinX <tamalhansda16@gmail.com>
2020-09-19 08:55:57 +0530

    windowed eye pixel sum

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Eye-Blink-Detection
 Javascript project to detect eye blink from video feed.
+
+
+In this method I'll try to get a window where eye should be placed.
+Then take sum of all pixel(grayscale) values inside that window to determine if eye is closed or not.
--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,13 @@ utils.loadOpenCv(()=>{
     // take param from the video input
     let height = video.height;
     let width = video.width;
-    let streaming = true;
+    let streaming = false;
+
+    let windowWidth = 60;
+    let windowHeight = 20;
+    let windowPoint1 = new cv.Point(width/2-windowWidth/2, height/2-windowHeight/2);
+    let windowPoint2 = new cv.Point(width/2+windowWidth/2, height/2+windowHeight/2);
+
 
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(function(stream) {
@@ -24,25 +30,7 @@ utils.loadOpenCv(()=>{
     let dst = new cv.Mat(height, width, cv.CV_8UC4);
     let gray = new cv.Mat();
     let cap = new cv.VideoCapture(video);
-    let faces = new cv.RectVector();
-    let eyes = new cv.RectVector();
 
-    let eyeClassifier = new cv.CascadeClassifier();  // initialize classifier
-    let faceClassifier = new cv.CascadeClassifier();  // initialize classifier
-
-    let eyeCascadeFile = 'haarcascade_eye.xml'; // path to xml
-    let faceCascadeFile = 'haarcascade_frontalface_default.xml'; // path to xml
-    // use createFileFromUrl to "pre-build" the xml
-    utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
-        
-        faceClassifier.load(faceCascadeFile); // in the callback, load the cascade from file 
-        console.log("cascade loaded face")
-    });
-    utils.createFileFromUrl(eyeCascadeFile, eyeCascadeFile, () => {
-        
-        eyeClassifier.load(eyeCascadeFile); // in the callback, load the cascade from file 
-        console.log("cascade loaded eye")
-    });
     const FPS = 30;
     function processVideo() {
         try {
@@ -51,43 +39,30 @@ utils.loadOpenCv(()=>{
                 src.delete();
                 dst.delete();
                 gray.delete();
-                faces.delete();
-                eyes.delete();
-                classifier.delete();
                 return;
             }
             let begin = Date.now();
             // start processing.
             cap.read(src);
-            //src.resizeTo = cv.Size(width/4, height/4)
-            //resizeTo();
             src.copyTo(dst);
             cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
-            // detect faces.
-            let msize = new cv.Size(0, 0);
-            faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0);
             
+            cv.rectangle(dst,windowPoint1,windowPoint2, [0, 255, 0, 255]);
 
-            // draw faces.
-            for (let i = 0; i < faces.size(); ++i) {
-                let face = faces.get(i);
-                let point1 = new cv.Point(face.x, face.y);
-                let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-                cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+            let windowImage = gray.roi(new cv.Rect(width/2-windowWidth/2, height/2-windowHeight/2, windowWidth, windowHeight))
 
-                //detect the eye inside the face box
-                let faceBox = gray.roi(new cv.Rect(face.x, face.y, face.width, face.height));
-                eyeClassifier.detectMultiScale(faceBox, eyes, 1.1, 3, 0);
-                
-                for (let i = 0; i < eyes.size(); ++i) {
-                    let eye = eyes.get(i);
-                    let point1 = new cv.Point(face.x+eye.x, face.y+eye.y);
-                    let point2 = new cv.Point(face.x+eye.x + eye.width, face.y+eye.y + eye.height);
-                    cv.rectangle(dst, point1, point2, [0, 255, 0, 255]);
-                }
-            }
-            
             cv.imshow('canvasOutput', dst);
+            cv.imshow('windowOutput', windowImage);
+
+            let sum = 0;
+            let prev = 0;
+            windowImage.data.forEach(element => {
+                let c = Math.round(element*8/255); 
+                prev != c ? sum += c: null;
+                prev = c
+            });
+            console.log(sum)
+
             // schedule the next one.
             let delay = 1000/FPS - (Date.now() - begin);
             setTimeout(processVideo, delay);
